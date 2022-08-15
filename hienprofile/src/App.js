697965779ae210913bbb5bf666c0baaac0eecfb9
Monil Gandhi <gandhi.monil007@gmail.com>
2022-08-15 19:08:02 +0530

    navbar is created

--- hienprofile/src/App.js
+++ hienprofile/src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import Footer from './components/Footer.js'; 
-import Header from './components/Header.js'; 
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/index';
+import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Navbar />
+      <Header />
       {/* main body */}
-      <Home/>
-      <About/>
+      <Home />
+      <About />
       {/* Footer */}
-      <Footer/>
+      <Footer />
     </div>
   );
 }
